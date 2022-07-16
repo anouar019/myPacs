573e8cca97693894c4eed99d588c2ecd6ed9f7e6
anouar019 <anouar019@gmail.com>
2022-07-16 22:11:04 +0200

    Commit message

--- src/main/java/cnam/medical/pacs/api/PatientUI.java
+++ src/main/java/cnam/medical/pacs/api/PatientUI.java
@@ -1,7 +1,10 @@
 package cnam.medical.pacs.api;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -12,20 +15,93 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.jboss.logging.Logger;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import cnam.medical.pacs.domain.dao.PatientRepo;
 import cnam.medical.pacs.domain.model.Patient;
+import cnam.medical.pacs.domain.model.Patient.Sex;
+
 
 @Path("/patient")
 public class PatientUI {
+
+    private Logger LOGGER=Logger.getLogger(PatientUI.class);
+
+    @Inject
+    PatientRepo patientRepo;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Patient> getPatients(){
 
-        return Patient.listAll();
+        LOGGER.info("Get List of Patients");
+        return patientRepo.listAll();
 
     }
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/id/{id}")
+    public Patient getPatient(@PathParam Long id){
+
+
+        LOGGER.info("Get Patient by id");
+        return patientRepo.findById(id);
+
+    }
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/lastname/{lastname}")
+    public List<Patient> getPatientByLastName(@PathParam String lastname){
+
+        LOGGER.info("Get Patients by lastname");
+        return patientRepo.findByLastName(lastname);
+
+    }
+
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/firstname/{firstname}")
+    public List<Patient> getPatientByFirstName(@PathParam String firstname){
+
+        LOGGER.info("Get Patients by firstname");
+        return patientRepo.findByFirstName(firstname);
+
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/sex/{sex}")
+    public List<Patient> getPatientBySex(@PathParam Sex sex){
+
+        LOGGER.info("Get Patients by sex");
+        return patientRepo.findBySex(sex);
+
+    }
     
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/birth/{birth}")
+    public List<Patient> getPatientByBirth(@PathParam String birth){
+
+
+        LOGGER.info("Get Patients by birthdate");
+
+        //convert String to LocalDate
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d");
+        LocalDate localDate = LocalDate.parse(birth, formatter);
+        
+        return patientRepo.findByBirth(localDate);
+        
+
+
+    }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -33,9 +109,16 @@ public class PatientUI {
     @Transactional
     public Response savePatient(Patient patient){
 
-        patient.persist();
+        LOGGER.info("Post Patient");
+        patientRepo.persist(patient);
         return Response.status(Status.CREATED).entity(patient).build();
 
     }
+
+
+
+
+
+ 
 
 }
--- src/main/java/cnam/medical/pacs/domain/model/Patient.java
+++ src/main/java/cnam/medical/pacs/domain/model/Patient.java
@@ -6,16 +6,22 @@ import javax.persistence.Entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
+
+/** 
+   * Patient is my Patient model uses to store information about patients identity to database
+   * This class extends PanacheEntity
+   */
+
 @Entity
 public class Patient extends PanacheEntity{
 
     public String lastName;
     public String firstName;
-    public LocalDate birthday;
+    public LocalDate birth;
     public Sex sex;
 
 
-    private enum Sex {
+    public enum Sex {
         MALE, FEMALE
       }
     
